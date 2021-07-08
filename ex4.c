e7bc6a6f25d22d92c852499963eb1f09d13ec332
s1280230 <87129000+s1280230@users.noreply.github.com>
2021-07-08 22:23:12 +0900

    名前を聞くパートを足した。

--- ex4.c
+++ ex4.c
@@ -3,7 +3,11 @@
 
 int main(){
    int i,num,heads=0,tails=0;
-
+   
+   char str[8];
+   printf("Who are you? ");
+   scanf("%s", str);
+   printf("Hello, %s\n", str);
    printf("Tossing a coin...\n");
 
    for(i=0;i<3;i++){
